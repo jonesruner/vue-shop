b916b4393c3fa7780ffbaf59dadd98da775e28ce
jone <jonesRuner@163.com>
2020-07-26 18:30:33 +0800

    update login and register page

---
+++ src/components/login.vue
@@ -0,0 +1,15 @@
+<template>
+   <div>
+      This is login page   adfafafa
+   </div>
+</template>
+<script>
+export default {
+
+}
+
+console.log('----------------------->enter in login')
+</script>
+<style scoped>
+
+</style>
---
+++ src/components/register.vue
@@ -0,0 +1,13 @@
+<template>
+    <h2>This is register page</h2>
+</template>
+
+<script>
+export default {
+
+}
+
+console.log('----------------------->enter in register')
+</script>
+<style scoped>
+</style>
